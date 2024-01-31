a589dca427103b07000c7b31b975f29872934b10
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2024-01-31 13:54:27 -0800

    feat: add mobile friendly hello

--- app/components/Hello.tsx
+++ app/components/Hello.tsx
@@ -1,10 +1,16 @@
 export default function Hello() {
   return (
-    <div className="px-28 flex min-h-[80vh] flex-col justify-center items-center">
-      <h1 className="font-dm text-8xl pb-8 pt-12">Caitlin Vandersluis</h1>
-      <h3 className="text-xl">
+    <div className="p-8 md:px-28 flex min-h-screen md:min-h-[80vh] flex-col justify-center items-center gap-8">
+      <h1 className="font-dm md:pt-14 text-center text-6xl md:text-8xl">
+        Caitlin Vandersluis
+      </h1>
+      <h3 className="text-center text-sm md:text-xl">
         Full Stack Web Developer based in Victoria, BC
       </h3>
+      <div className="flex flex-row items-center md:hidden text-sm gap-3">
+        <p>github</p>
+        <p>linkedin</p>
+      </div>
     </div>
   );
 }
