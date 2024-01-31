63a4100d4989cb78b07e004ffe125e9be91ff030
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2024-01-30 21:37:02 -0800

    add: landing page

---
+++ app/components/Hello.tsx
@@ -0,0 +1,10 @@
+export default function Hello() {
+  return (
+    <div className="px-28 flex min-h-[80vh] flex-col justify-center items-center">
+      <h1 className="font-dm text-8xl pb-8 pt-12">Caitlin Vandersluis</h1>
+      <h3 className="text-xl">
+        Full Stack Web Developer based in Victoria, BC
+      </h3>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import Hello from "./components/Hello";
+import Nav from "./components/Nav";
+import AboutMe from "./components/AboutMe";
+import Projects from "./components/Projects";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-between p-24">
-      <h1>Caitlin Vandersluis</h1>
+    <main className="bg-light">
+      <div className="container mx-auto">
+        <Hello />
+        <Nav />
+        <AboutMe />
+        <Projects />
+      </div>
     </main>
   );
 }
