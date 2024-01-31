56f1ce1c110d9af26b4cfd5987d58c5cf803308e
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2024-01-31 13:54:07 -0800

    feat: add animation on scroll

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,33 @@
+"use client";
+
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import Hello from "./components/Hello";
 import Nav from "./components/Nav";
 import AboutMe from "./components/AboutMe";
 import Projects from "./components/Projects";
 
 export default function Home() {
+  useEffect(() => {
+    AOS.init({
+      duration: 600,
+      once: false,
+    });
+  }, []);
+
   return (
     <main className="bg-light">
       <div className="container mx-auto">
         <Hello />
         <Nav />
-        <AboutMe />
-        <Projects />
+        <div data-aos="fade-up">
+          <AboutMe />
+        </div>
+        <div data-aos="fade-up">
+          <Projects />
+        </div>
       </div>
     </main>
   );
