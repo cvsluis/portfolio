ba843af91f7d63ecedd541240348808bdf2a8342
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2024-02-01 19:50:45 -0800

    fix: font size and margin

--- app/components/Hello.tsx
+++ app/components/Hello.tsx
@@ -3,9 +3,9 @@ import LinkedIn from "../svg/LinkedIn";
 
 export default function Hello() {
   return (
-    <div className="p-8 md:px-28 flex min-h-screen md:min-h-[80vh] flex-col justify-center items-center gap-12">
+    <div className="p-8 md:py-0 md:px-28 flex min-h-screen md:min-h-[80vh] flex-col justify-center items-center gap-12">
       <div className="flex flex-col gap-8">
-        <h1 className="font-dm md:pt-36 text-center text-6xl md:text-8xl">
+        <h1 className="font-dm md:pt-48 text-center text-6xl md:text-8xl">
           Caitlin Vandersluis
         </h1>
         <h3 className="text-center text-sm md:text-xl">
