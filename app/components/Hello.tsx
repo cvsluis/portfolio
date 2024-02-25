d8ce9e4ee3b099d82d1a74ca1888903956963abd
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2024-02-24 21:50:34 -0800

    fix: hello title

--- app/components/Hello.tsx
+++ app/components/Hello.tsx
@@ -1,22 +1,22 @@
-import Github from "../svg/Github";
-import LinkedIn from "../svg/LinkedIn";
+import Github from "../ui/svg/Github";
+import LinkedIn from "../ui/svg/LinkedIn";
 
 export default function Hello() {
   return (
     <div className="p-8 md:py-0 md:px-28 flex min-h-screen md:min-h-[80vh] flex-col justify-center items-center gap-12">
       <div className="flex flex-col gap-8">
-        <h1 className="font-dm md:pt-48 text-center text-6xl md:text-8xl">
-          Caitlin Vandersluis
+        <h1 className="font-outfit font-bold md:pt-48 text-center text-6xl md:text-8xl">
+          Hi, I'm Caitlin!
         </h1>
         <h3 className="text-center text-sm md:text-xl">
           Full Stack Web Developer based in Victoria, BC
         </h3>
         <div className="flex flex-row justify-center items-center md:hidden text-sm gap-3">
-          <Github />
+          <Github link="https://github.com/cvsluis" />
           <LinkedIn />
         </div>
       </div>
-      <img src="/images/caitlin.png" className="max-w-full h-auto"></img>
+      <img src="/images/caitlin-emoji.png" className="w-[260px] h-auto"></img>
     </div>
   );
 }
