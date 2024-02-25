5e86377b611aaf4cf03938122fe8d39e51ff48f1
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2024-02-24 17:20:01 -0800

    add: top and bottom nav

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -2,28 +2,39 @@ import { Metadata } from "next";
 import { projects } from "../../lib/data";
 import Carousel from "@/app/ui/project/carousel";
 import Details from "@/app/ui/project/details";
+import Navigation from "@/app/ui/project/navigation";
+import Asterisk from "@/app/ui/svg/Asterisk";
+import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: "Projects",
+  title: "Caitlin Vandersluis | Projects",
 };
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = Number(params.id);
   const project = projects.find((project) => project.id === id);
+  const numOfProjects = projects.length;
 
   if (!project) {
     return <div>Project not found!</div>;
   }
 
   return (
-    <main className="min-h-screen flex flex-col justify-center">
-      {/* title */}
-      {/* close button */}
-      <div className="p-4 md:p-0 container mx-auto flex flex-col md:flex-row justify-center items-center gap-8">
+    <main className="container mx-auto p-4 md:py-16 min-h-screen flex flex-col justify-between gap-4 md:gap-0">
+      <div className="flex flex-row justify-between">
+        <div className="flex flex-row items-center gap-2">
+          <Asterisk />
+          PROJECTS
+        </div>
+        <Link href="/#projects">X</Link>
+      </div>
+
+      <div className="container mx-auto flex flex-col md:flex-row justify-center items-center gap-8">
         <Carousel images={project.images} />
         <Details {...project} />
       </div>
-      {/* bottom navigation */}
+
+      <Navigation id={id} name={project.name} numOfProjects={numOfProjects} />
     </main>
   );
 }
