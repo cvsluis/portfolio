97c4b64a66c1c80a7bf9f6ba928a7ce1785b331c
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2024-02-24 21:49:26 -0800

    fix: update filenames

--- app/fonts.js
+++
@@ -1,9 +0,0 @@
-import { Inter, DM_Serif_Display, Montserrat } from "next/font/google";
-
-export const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
-export const dm_serif_display = DM_Serif_Display({
-  subsets: ["latin"],
-  weight: "400",
-  variable: "--font-dm",
-});
-export const montserrat = Montserrat({ subsets: ["latin"], weight: "700", variable: "--font-montserrat" });
